e5322965d97b9ade6c22bc730dd461b9344a60b9
ImamIlyasa12 <46297874+ImamIlyasa12@users.noreply.github.com>
2019-01-02 22:13:30 +0700

    Create DefaceWebdav.sh

---
+++ DefaceWebdav.sh
@@ -0,0 +1,122 @@
+#Kalo mau recode izin dulu sama author
+#Bisa hubungi lewat blog: https://imamilyasa.blogspot.com/
+#Author: ./Z5X_ID
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+ungu="\033[35;1m"
+cyan="\033[36;1m"
+white="\033[37;1m"
+red="\033[31;1m"
+clear
+echo
+echo
+echo
+echo
+echo $red"        TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+echo $red"                     HARAP SABAR MENUNGGU :)"   
+sleep 1
+echo '''\a
+\033[34;1m                              _
+\033[34;1m                             / |
+\033[34;1m                             | |
+\033[34;1m                             | |
+\033[34;1m                             |_| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"        TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+echo $red"                     HARAP SABAR MENUNGGU :)"
+sleep 1
+echo '''\a
+\033[32;1m                             ____
+\033[32;1m                            |___ \
+\033[32;1m                             __) |
+\033[32;1m                            / __/
+\033[32;1m                           |_____| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"        TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+echo $red"                     HARAP SABAR MENUNGGU :)"
+sleep 1
+echo '''\a
+\033[35;1m                            _____
+\033[35;1m                           |___ /
+\033[35;1m                             |_ \
+\033[35;1m                            ___) |
+\033[35;1m                           |____/ '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"        TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+echo $red"                     HARAP SABAR MENUNGGU :)"
+sleep 1
+echo '''\a
+\033[33;1m                            _  _
+\033[33;1m                           | || |
+\033[33;1m                           | || |_
+\033[33;1m                           |__   _|
+\033[33;1m                              |_| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"        TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+echo $red"                     HARAP SABAR MENUNGGU :)"
+sleep 1
+echo '''\a
+\033[36;1m                             ____
+\033[36;1m                            | ___|
+\033[36;1m                            |___ \
+\033[36;1m                             ___) |
+\033[36;1m                            |____/ '''
+sleep 1
+clear
+echo
+echo $red
+pkg install toilet -y
+clear
+echo $green
+pkg install figlet -y
+clear
+echo $cyan
+pkg install ruby -y
+clear
+gem install figlet
+clear
+gem install lolcat
+clear
+pkg install curl
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+sleep 1
+toilet -f future "    Install selesai" | lolcat
+sleep 5
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+toilet -f future " Wellcome to my tools" | lolcat
+sleep 5
+clear
+sh 77
